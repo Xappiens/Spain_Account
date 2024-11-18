d10c25ef3cd16373e80ccfba89eae53d53ccd3c9
Bansi2002 <bansi@sanskartechnolab.com>
2024-11-18 13:29:57 +0530

    chore :update tax rate in purchase invoice

--- spain_account/spain_accounting/py/purchase_invoice.py
+++ spain_account/spain_accounting/py/purchase_invoice.py
@@ -5,11 +5,15 @@ from erpnext.controllers.taxes_and_totals import calculate_taxes_and_totals
 def modify_tds_tax_row(doc, method):
 
     if doc.apply_tds :
+        tax_withholding_category = frappe.db.get_value("Tax Withholding Rate", {"parent":doc.tax_withholding_category},"tax_withholding_rate")
         for tax_row in doc.taxes:
+            if tax_row.is_tax_withholding_account:
+                tax_row.rate = tax_withholding_category
                 if tax_row.charge_type == "Actual":  
                     tax_row.charge_type = "On Net Total"  
-                    tax_row.save()  
-    
+                tax_row.save()  
+        
         # RE-CALCULATE TAXES AND TOTALS AFTER MODIFYING THE CHARGE_TYPE
         calculate_taxes_and_totals(doc)
         frappe.db.commit()
+        doc.reload()
