51f6c3ae2749240ce0e26919fdf1d19aebec8962
maxinisalo97 <maxinisalo97@gmail.com>
2025-01-17 12:37:18 +0000

    Updating Amortization Logic

--- spain_account/spain_accounting/doctype/amortization/amortization.js
+++ spain_account/spain_accounting/doctype/amortization/amortization.js
@@ -9,6 +9,42 @@ frappe.ui.form.on("Amortization", {
                 }
             };
         });
+        frm.set_query("invoice", function() {
+            return {
+                filters: {
+                    company: frm.doc.company || null // Enviar la empresa si está seleccionada
+                }
+            };
+        });
+        frm.fields_dict.invoice.df.onchange = function() {
+            if (!frm.doc.company && frm.doc.invoice) {
+                frappe.db.get_value("Purchase Invoice", frm.doc.invoice, "company", function(value) {
+                    if (value && value.company) {
+                        frm.set_value("company", value.company);
+                    }
+                });
+            }
+            if (!frm.doc.purchase_date && frm.doc.invoice) {
+                frappe.db.get_value("Purchase Invoice", frm.doc.invoice, "posting_date", function(value) {
+                    if (value && value.posting_date) {
+                        frm.set_value("purchase_date", value.posting_date);
+                    }
+                });
+            }
+            if (!frm.doc.price && frm.doc.invoice) {
+                frappe.db.get_value("Purchase Invoice", frm.doc.invoice, "net_total", function(value) {
+                    if (value && value.net_total) {
+                        frm.set_value("price", value.net_total);
+                    }
+                });
+            }
+        }
+
+        frm.fields_dict.price.df.onchange = function() {
+            if (!frm.doc.amortizable_value && frm.doc.price) {
+                frm.set_value("amortizable_value", frm.doc.price);
+            }
+        }
         // Detectar cambios en el campo account
         frm.fields_dict.account.df.onchange = function() {
             if (!frm.doc.company && frm.doc.account) {
--- spain_account/spain_accounting/doctype/amortization/amortization.py
+++ spain_account/spain_accounting/doctype/amortization/amortization.py
@@ -1,39 +1,77 @@
 # Copyright (c) 2025, Xappiens and contributors
 # For license information, please see license.txt
+from datetime import datetime
 import frappe
 from frappe.model.document import Document
-from frappe.utils import flt, get_datetime, getdate, add_days, add_months, add_years
+from frappe.utils import get_datetime
+
 class Amortization(Document):
-	
-	def on_update(self):
-		self.calculate_amortization()
-	@frappe.whitelist()
-	def calculate_amortization(self):
-		if not self.price or not self.percentage or not self.purchase_date:
-			frappe.throw("Debe ingresar los campos obligatorios")
-		self.amortization_details = []
-		# Calculo de la amortizacion
-		annual_amount = (self.price * self.percentage) / 100
-		accumulated_amount = self.initial_amortization or 0
-		year = get_datetime(self.purchase_date).year
-		if self.initial_amortization:
-			self.append("amortization_details", {
-				"year": year,
-				"annual_amount": self.initial_amortization,
-				"accumulated_amount": accumulated_amount
-			})
-			year += 1
-		# Generar las filas de amortizacion
-		while accumulated_amount < self.price:
-			remaining_amount = self.price - accumulated_amount
-			amortized_on_year = min(annual_amount, remaining_amount)  # Ajustar el último año si es menor que el monto anual
-			self.append("amortization_details", {
-				"year": year,
-				"annual_amount": amortized_on_year,  # Monto amortizado este año
-				"accumulated_amount": accumulated_amount + amortized_on_year  # Monto acumulado actualizado
-			})
-			accumulated_amount += amortized_on_year
-			year += 1
+    
+    def on_update(self):
+        self.calculate_amortization()
+    
+    @frappe.whitelist()
+    def calculate_amortization(self):
+        if not self.amortizable_value or not self.percentage or not self.start_date:
+            frappe.throw("Debe ingresar los campos obligatorios")
+        
+        self.amortization_details = []  # Limpiar detalles previos
+        
+        # Calcular el saldo total a amortizar
+        total_amortizable = self.amortizable_value  
+        accumulated_amount = 0  # Monto acumulado
+
+        # Convertir fechas a objetos datetime
+        start_date = get_datetime(self.start_date)
+        end_date = get_datetime(self.end_date) if self.end_date else None
+
+        current_year = start_date.year
+        remaining_amount = total_amortizable  # Saldo pendiente
+        annual_max_amortization = total_amortizable * (self.percentage / 100)  # Monto máximo por año
+
+        while remaining_amount > 0:
+            # Determinar el rango de fechas del año actual
+            year_start = datetime(current_year, 1, 1)
+            year_end = datetime(current_year, 12, 31)
+            
+            # Ajustar si la amortización comienza a mitad de año
+            if current_year == start_date.year:
+                year_start = start_date
+            
+            # Ajustar si hay una fecha de finalización
+            if end_date and end_date.year == current_year:
+                year_end = end_date
+            
+            # Calcular días activos en el año
+            days_in_year = (datetime(current_year, 12, 31) - datetime(current_year, 1, 1)).days + 1
+            days_active = (year_end - year_start).days + 1
+            
+            # Calcular amortización para este año en base a los días activos
+            annual_amount = (annual_max_amortization / days_in_year) * days_active
+            annual_amount = min(annual_amount, remaining_amount)  # No exceder el saldo pendiente
+            
+            # Calcular porcentaje amortizado en este año
+            annual_percentage = (annual_amount / total_amortizable) * 100
+            
+            # Registrar la amortización del año actual
+            self.append("amortization_details", {
+                "year": current_year,
+                "annual_amount": round(annual_amount, 2),
+                "accumulated_amount": round(accumulated_amount + annual_amount, 2),
+                "percentage": round(annual_percentage, 2)  # Nuevo campo agregado
+            })
+            
+            # Actualizar valores
+            accumulated_amount += annual_amount
+            remaining_amount -= annual_amount
+            
+            # Detener si alcanzamos la fecha de finalización
+            if end_date and current_year >= end_date.year:
+                break
+            
+            # Pasar al siguiente año
+            current_year += 1
+
 @frappe.whitelist()
 def get_filtered_accounts(doctype, txt, searchfield, start, page_len, filters):
     """
