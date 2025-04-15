7a387074e72f26e57b15bd8e0864dafb1b96ab82
mehedi432 <aamehedi93@gmail.com>
2025-04-15 11:32:28 +0000

    Elimina el módulo de justification de spain_account

--- spain_account/justification/doctype/acciones_bolsa/acciones_bolsa.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Xappiens and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Acciones Bolsa", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- spain_account/justification/doctype/gastos_por_sede/gastos_por_sede.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Xappiens and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Gastos por Sede", {
-// 	refresh(frm) {
-
-// 	},
-// });
